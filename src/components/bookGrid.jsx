3300564378be3e618ce8398107cc5fed5baf2b66
rahafo <rahaf.o.omar@gmail.com>
2021-01-05 00:39:32 +0300

    fix key error in books grid

--- src/components/bookGrid.jsx
+++ src/components/bookGrid.jsx
@@ -7,8 +7,8 @@ export default class BookGrid extends Component {
             <ol className="books-grid">
                 {
                     this.props.books.map((book)=>
-                        <li>
-                            <Book book={book} key={book.title}/>
+                        <li key={book.title}>
+                            <Book book={book}/>
                         </li>
                         )
                 }
