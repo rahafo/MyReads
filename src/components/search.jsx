f9ede226d922f71b200bfa085b578b33b24a9219
rahafo <rahaf.o.omar@gmail.com>
2021-01-06 23:46:48 +0300

    book search

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,20 +1,47 @@
 import React, {Component} from 'react'
-import BookGrid from "./bookShelf";
+import {BookGrid} from "./bookGrid";
 import {Link} from 'react-router-dom'
+import {search, update} from '../BooksAPI'
 
 export default class Search extends Component {
+
+    state = {
+        searchTerm: "",
+        books: []
+    };
+
+    onSearchChange = (searchTerm) => {
+        this.setState({searchTerm}, () => this.searchForBook())
+    };
+
+    searchForBook = () => {
+        search(this.state.searchTerm).then((res) => this.setState({books: res}))
+    };
+
+    updateBook = (book, shelf) => {
+        update(book, shelf)
+    };
+
     render() {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <Link to="./" ><button className="close-search">Close</button></Link>
+                    <Link to="./">
+                        <button className="close-search">Close</button>
+                    </Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author"/>
-
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={this.state.searchTerm} onChange={(event) => this.onSearchChange(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <BookGrid/>
+                    <BookGrid
+                        books={this.state.books}
+                        updateBook={this.updateBook}
+                    />
                 </div>
             </div>
         )
